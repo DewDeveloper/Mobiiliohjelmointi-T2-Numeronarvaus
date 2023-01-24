8f9158e1a6c7026c601f6ca0efac08b07f777253
Matias Karila <matias.karila@myy.haaga-helia.fi>
2023-01-24 14:16:04 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { react } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text1, setText1] = useState('Guess a number between 1-100');
+
+  const [inp, setInp] = useState("");
+
+  const [randNum, setRandNum] = useState(0);
+
+  const [generated, setGenerated] = useState(false);
+
+  const [count, setCount] = useState(0);
+
+  if (generated == false) {
+    var numero = (Math.floor(Math.random() * 100) + 1);
+    setRandNum(numero);
+    setGenerated(true);
+  }
+
+  function check() {
+    setCount(count + 1);
+    var intGuess = parseInt(inp);
+    if (intGuess == randNum) {
+      Alert.alert("You guessed the number in " + count + " guesses");
+    } 
+    else if (intGuess <= randNum) {
+      Alert.alert("Your guess " + intGuess + " is too low");
+    }
+    else {
+      Alert.alert("Your guess " + intGuess + " is too high")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text>{text1}</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setInp}
+        value={inp}
+        keyboardType="numeric"
+      />
+
+      <Button
+        style={styles.button}
+        title="MAKE GUESS"
+        color="blue"
+        onPress={check}
+      />
+
+
     </View>
   );
 }
@@ -16,5 +65,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 30,
+    width: 150,
+    margin: 5,
+    borderWidth: 2,
+    padding: 5,
+  },
+  button: {
+    height: 20,
+    width: 50,
+    justifyContent: "center",
+    padding: 150,
   },
 });
